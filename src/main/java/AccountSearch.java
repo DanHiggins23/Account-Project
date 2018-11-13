ea839b98affd763bb34b301fb688f96a6d0cdcb5
DanHiggins23 <danhiggins23@outlook.com>
2018-11-13 15:01:59 +0000

    Second commit - tests passed java 7

--- src/main/java/AccountSearch.java
+++ src/main/java/AccountSearch.java
@@ -1,6 +1,16 @@
+import java.util.Map;
+
 public class AccountSearch {
     public int searchAccounts(String searchName) {
+        Service s = new Service();
         int count = 0;
+
+        for (Map.Entry<Integer, Account> entry : s.accounts.entrySet()) {
+            if (entry.getValue().getFirstName().equals(searchName) || entry.getValue().getSecondName().equals(searchName)) {
+                count ++;
+            }
+        }
+
         return count;
     }
 }
--- src/main/java/Service.java
+++ src/main/java/Service.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 
 class Service {
-    HashMap<Integer, Account> accounts = new HashMap<Integer, Account>();
+    static HashMap<Integer, Account> accounts = new HashMap<Integer, Account>();
 
     public void createAccount(Account person) {
         accounts.put(person.getAccountNum(), person);
